6861ca5c0354d299006e423735f0fde6a20ac477
FELIPE CASTRO SILVA <felipecastrosilva.27@gmail.com>
2025-08-02 16:04:17 -0300

    refactor: melhorias gerais no código
    - Ajusta as exceções na ContaService utilizando exceções personalizadas.
    - Abstrai a lógica de validação do formato da transação para um método específico.
    - Altera a lógica de cálculo da taxa de cada transação.

--- src/main/java/com/felipe/gestaoBancaria/service/ContaService.java
+++ src/main/java/com/felipe/gestaoBancaria/service/ContaService.java
@@ -1,12 +1,17 @@
 package com.felipe.gestaoBancaria.service;
 
+import com.felipe.gestaoBancaria.exception.*;
 import com.felipe.gestaoBancaria.model.Conta;
 import com.felipe.gestaoBancaria.repository.ContaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class ContaService
 {
+    private static final Map<String, Double> TAXA_POR_FORMA_PAGAMENTO = Map.of("P", 0.0, "D", 0.03, "C", 0.05);
+
     private final ContaRepository contaRepository;
 
     public ContaService(ContaRepository contaRepository)
@@ -14,7 +19,7 @@ public class ContaService
         this.contaRepository = contaRepository;
     }
 
-    public Conta criarConta(int numeroConta, double saldoInicial) throws IllegalArgumentException
+    public Conta criarConta(int numeroConta, double saldoInicial)
     {
         validarNovaConta(numeroConta, saldoInicial);
 
@@ -27,21 +32,18 @@ public class ContaService
     {
         if (saldoInicial < 0)
         {
-            throw new IllegalArgumentException("Saldo da Conta não pode ser negativo.");
+            throw new SaldoNegativoException("Uma conta não pode ser criada com saldo negativo.");
         }
 
         if (contaRepository.existeConta(numeroConta))
         {
-            throw new IllegalArgumentException("Conta já existe.");
+            throw new ContaExistenteException("A conta de número: " + numeroConta + " já existe na base de dados.");
         }
     }
 
-    public Conta realizarTransacao(int numeroConta, String formaPagamento, double valor) throws IllegalArgumentException
+    public Conta realizarTransacao(int numeroConta, String formaPagamento, double valor)
     {
-        if (valor <= 0)
-        {
-            throw new IllegalArgumentException("O valor da transação deve ser positivo.");
-        }
+        validarFormatoTransacao(formaPagamento, valor);
 
         Conta conta = consultarConta(numeroConta);
 
@@ -55,13 +57,28 @@ public class ContaService
         return contaRepository.salvar(conta);
     }
 
-    public Conta consultarConta(int numeroConta) throws IllegalArgumentException
+    private static void validarFormatoTransacao(String formaPagamento, double valor)
+    {
+        if (valor <= 0)
+        {
+            throw new ValorTransacaoInvalidoException("O valor de qualquer transação deve ser maior que zero.");
+        }
+
+        if (!TAXA_POR_FORMA_PAGAMENTO.containsKey(formaPagamento))
+        {
+            String formasAceitas = String.join(", ", TAXA_POR_FORMA_PAGAMENTO.keySet());
+
+            throw new FormaPagamentoInvalidaException("Formas aceitas: " + formasAceitas);
+        }
+    }
+
+    public Conta consultarConta(int numeroConta)
     {
         Conta conta = contaRepository.buscarContaPorNumero(numeroConta);
 
         if (conta == null)
         {
-            throw new IllegalArgumentException("Conta não encontrada.");
+            throw new ContaNaoEncontradaException("A conta de número: " + numeroConta + " não foi encontrada na base de dados.");
         }
 
         return conta;
@@ -69,21 +86,14 @@ public class ContaService
 
     private double calcularTaxa(String formaPagamento, double valor)
     {
-        return switch (formaPagamento)
-        {
-            case "P" -> 0;
-            case "D" -> valor * 0.03;
-            case "C" -> valor * 0.05;
-
-            default -> throw new IllegalArgumentException("Forma de pagamento inválida.");
-        };
+        return valor * TAXA_POR_FORMA_PAGAMENTO.get(formaPagamento);
     }
 
     private static void validarTransacao(Conta conta, double total)
     {
         if (conta.getSaldo() < total)
         {
-            throw new IllegalArgumentException("Saldo insuficiente.");
+            throw new SaldoInsuficienteException("Saldo: " + conta.getSaldo() + " Valor: " + total);
         }
     }
 }
